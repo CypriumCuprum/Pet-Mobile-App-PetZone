3d7a9b62bbb84af7319907e8257a51e2cbd7e0ac
nt.hoa141203@gmail.com <nt.hoa141203@gmail.com>
2025-03-05 10:42:40 +0700

    up style  topbar

--- app/src/main/java/com/example/petapp/MainActivity.kt
+++ app/src/main/java/com/example/petapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.petapp
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pet_health)
+        // Thiết lập để nội dung vẽ dưới status bar
+        window.setDecorFitsSystemWindows(false)
+        // Làm status bar trong suốt
+        window.statusBarColor = Color.TRANSPARENT
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
