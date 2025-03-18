93c738d4bcd651413bef3101bef2f4ca431af9d8
BunRealCua123 <hieukv26102003@gmail.com>
2025-03-18 09:02:08 +0700

    reminder

--- app/src/main/java/com/example/petapp/MainActivity.kt
+++ app/src/main/java/com/example/petapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.petapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT // Làm status bar trong suốt
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
