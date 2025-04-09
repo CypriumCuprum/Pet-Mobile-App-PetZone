06a656a992d0480a7421d2b1b87cf561770fefd0
CypriumCuprum <riverflowsinyou1412@gmail.com>
2025-04-09 10:21:56 +0700

    adding pet

--- app/src/main/java/com/example/petapp/data/repository/PetRepository.kt
+++ app/src/main/java/com/example/petapp/data/repository/PetRepository.kt
@@ -1,7 +1,11 @@
 package com.example.petapp.data.repository
 
+import androidx.room.Insert
 import com.example.petapp.data.local.dao.PetDAO
+import com.example.petapp.data.model.PetEntity
 
 class PetRepository(private val petDAO: PetDAO) {
     suspend fun getPetById(userid: String) = petDAO.getPetById(userid)
+    
+    suspend fun addPet(petEntity: PetEntity) = petDAO.createPet(petEntity)
 }
---
+++ app/src/main/java/com/example/petapp/viewmodel/pet/PetViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.petapp.viewmodel.pet
+
+import com.example.petapp.data.repository.PetRepository
+
+class PetViewModel(private val repository: PetRepository) {
+}
